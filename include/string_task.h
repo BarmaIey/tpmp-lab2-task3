5db6a83060c0fcdb81a4ae45cf14e89fe10c0415
Barmaley <dakos4k@gmail.com>
2026-03-22 21:57:47 +0300

    changed the project structure due to new info

--- include/string_task.h
+++
@@ -1,7 +0,0 @@
-#ifndef STRING_TASK_H
-#define STRING_TASK_H
-
-void insert_ascii_codes(char *input, char *output);
-
-#endif
-
--- src/main.c
+++ src/main.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "string_task.h"
 #include "route.h"
 
 int main()
 {
-    char input[200];
-    char output[600];
-
-    printf("Enter string: ");
-    fgets(input, sizeof(input), stdin);
-
-    input[strcspn(input, "\n")] = 0;
-
-    insert_ascii_codes(input, output);
-
-    printf("Result: %s\n", output);
-
     MARSHRUT routes[MAX_ROUTES];
     int n = input_routes(routes);
 
